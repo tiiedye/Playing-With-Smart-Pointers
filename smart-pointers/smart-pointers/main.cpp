6f3541c8d1332626488e1686a797cfe96a9e6159
tiiedye <tiiedye@gmail.com>
2020-09-13 10:16:07 -0700

    adding source code and updating readme

--- smart-pointers/smart-pointers/main.cpp
+++ smart-pointers/smart-pointers/main.cpp
@@ -1,20 +1,29 @@
-// smart-pointers.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <memory>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Test {
+private:
+    int data;
+public:
+    Test() : data{ 0 } { std::cout << "\tTest constructor (" << data << ")" << std::endl; }
+    Test(int data) : data{ data } { std::cout << "\tTest constructor (" << data << ")" << std::endl; }
+    int get_data() const { return data; }
+    ~Test() { std::cout << "\tTest destructor (" << data << ")" << std::endl; }
+};
+
+// Function prototypes
+std::unique_ptr<std::vector<std::shared_ptr<Test>>> make();
+void fill(std::vector<std::shared_ptr<Test>>& vec, int num);
+void display(const std::vector<std::shared_ptr<Test>>& vec);
+
+int main() {
+    std::unique_ptr<std::vector<std::shared_ptr<Test>>> vec_ptr;
+    vec_ptr = make();
+    std::cout << "How many data points do you want to enter: ";
+    int num;
+    std::cin >> num;
+    fill(*vec_ptr, num);
+    display(*vec_ptr);
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
